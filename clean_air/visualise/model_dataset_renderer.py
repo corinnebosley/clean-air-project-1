49d80c7cea0901889845ef072a61cbaeaa5d962c
corinnebosley <corinne.bosley@metoffice.gov.uk>
2020-11-12 14:03:13 +0000

    completed setup with sample data and added environment yaml.

--- clean_air/visualise/model_dataset_renderer.py
+++ clean_air/visualise/model_dataset_renderer.py
@@ -1,10 +1,21 @@
-##This just an example file for demo purposes
+"""
+Top-level module for rendering datasets.
+"""
 
-##Class to render obs datasets
-class ModelDatasetRenderer():
+import matplotlib as mpl
+import datashader as ds
+import pandas as pd
+
+MODEL_DATA_PATH = "~cbosley/Projects/toybox/cap_sample_data/model/"
+OBS_DATA_PATH = "~cbosley/Projects/toybox/cap_sample_data/obs/"
+AIRCRAFT_DATA_PATH = "~cbosley/Projects/toybox/cap_sample_data/aircraft/"
+
+
+class ModelDatasetRenderer:
     def __init__(self, dataset):
         self.dataset = dataset
 
     def render(self):
-        #call some nice drawing tools here and plot the details that you want to see in UI
+        # call some nice drawing tools here and plot the details that you
+        # want to see in UI
         pass
