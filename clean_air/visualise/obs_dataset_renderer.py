fe371266c8b677dcd6fb21bf8f8cc6133dd44609
corinnebosley <corinne.bosley@metoffice.gov.uk>
2020-11-12 14:03:13 +0000

    completed setup with sample data and added environment yaml.

--- clean_air/visualise/obs_dataset_renderer.py
+++ clean_air/visualise/obs_dataset_renderer.py
@@ -6,16 +6,18 @@ class ObsDatasetRenderer():
         self.dataset = dataset
 
     def render(self):
-        #This func will include an if statement to check object type before drawing it. eg: if shapefile then renderShapefile etc
+        # This func will include an if statement to check object type before
+        # drawing it. eg: if shapefile then renderShapefile etc
 
-        #EXAMPLE: call some nice drawing tools here and plot the details that you want to see in UI
+        # EXAMPLE: call some nice drawing tools here and plot the details
+        # that you want to see in UI
         self.nice_pic()
         self.line_graph()
 
-    def nice_pic():
-        #draw a pretty picture
+    def nice_pic(self):
+        # draw a pretty picture
         pass
 
-    def line_graph():
-        #draw a line graph
+    def line_graph(self):
+        # draw a line graph
         pass
