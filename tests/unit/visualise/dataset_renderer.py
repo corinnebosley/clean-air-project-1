d4ac995665d0e3008e5245887e2f7cef91936f29
corinnebosley <corinne.bosley@metoffice.gov.uk>
2020-11-16 12:03:33 +0000

    last currently required unit test for DatasetRenderer complete

--- tests/unit/visualise/dataset_renderer.py
+++ tests/unit/visualise/dataset_renderer.py
@@ -3,6 +3,8 @@ Unit tests for dataset_renderer.py
 """
 
 import clean_air.visualise.dataset_renderer as dr
+from clean_air.visualise import render_map, render_plot
+import iris
 import xarray
 
 MODEL_DATA_PATH = "/net/home/h06/cbosley/Projects/toybox/cap_sample_data/model/"
@@ -37,6 +39,25 @@ class TestRenderCall:
     """
     Class to test 'render' method of DatasetRenderer
     """
+    def setup_class(self):
+        self.data_path = MODEL_DATA_PATH + 'aqum_daily_daqi_mean_20200520.nc'
+#        self.multiple_dims = dr.DatasetRenderer(self.data_path).render()
+        # We need to test that we can make plots (instead of maps) if the
+        # number of dimension coordinates is less than 2, so we need to make a
+        # dataset with only 1 dim coord so that we can test the renderer's
+        # ability to determine the type of plot to produce:
+        tmp_cube = iris.load_cube(self.data_path)
+        self.y_slice = tmp_cube.extract(iris.Constraint(
+            projection_y_coordinate=-184000.00000))
+        # TODO: Determine the correct min values for the following coordinates:
+        # self.x_slice = tmp_cube.extract(iris.Constraint(
+        #     projection_x_coordinate=0))
+        # self.t_slice = tmp_cube.extract(iris.Constraint(time=0))
 
-
+    def test_render_map(self):
+        # Check that if the data has more than 1 dimension coordinate, the
+        # renderer chooses to create a map rather than a plot.
+        dframe = dr.DatasetRenderer(self.data_path)
+        dframe.render()
+        assert dframe.img_type == 'map'
 
