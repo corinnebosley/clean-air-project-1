eb2c6658d797fc869881ef43c091281d967d46f7
corinnebosley <corinne.bosley@metoffice.gov.uk>
2020-11-17 13:08:34 +0000

    completed plot-choosing process and added all possible tests at this stage.

--- clean_air/visualise/dataset_renderer.py
+++ clean_air/visualise/dataset_renderer.py
@@ -14,30 +14,58 @@ class DatasetRenderer:
         self.dims = self.dataset.dim_coords
 
         # Guess all possible dim coords here using iris object before loading
-        # dataframe as xarray object:
+        # dataframe as xarray object (but scalar coords become None because we
+        # can't make plots out of them):
         for coord in self.dataset.coords():
             axis = iris.util.guess_coord_axis(coord)
             if axis == 'X':
-                self.x_coord = coord.name()
+                if len(coord.points) > 1:
+                    self.x_coord = coord.name()
+                else:
+                    self.x_coord = None
             elif axis == 'Y':
-                self.y_coord = coord.name()
+                if len(coord.points) > 1:
+                    self.y_coord = coord.name()
+                else:
+                    self.y_coord = None
             elif axis == 'Z':
-                self.z_coord = coord.name()
+                if len(coord.points) > 1:
+                    self.z_coord = coord.name()
+                else:
+                    self.z_coord = None
             elif axis == 'T':
-                self.t_coord = coord.name()
+                if len(coord.points) > 1:
+                    self.t_coord = coord.name()
+                else:
+                    self.t_coord = None
 
         # Now load dataset as xarray object as this can read netcdf format
         # and also use the hvplot method:
         self.dataframe = xarray.open_dataset(dataset)
 
     def render(self):
-        # Need to know dimensionality of dataset here so that we know whether
-        # to call render_plot or render_map.
-        # Start with two dimensions only, then add checks and amendments to
-        # accommodate different dimensionalities:
-        if len(self.dims) > 1:
+        """
+        Analyses the dimensionality of the dataset and then sends to
+        appropriate renderer in render_plot.py or render_map.py.
+        """
+        # If we have both an x-coord and y-coord then we can draw a map:
+        if self.x_coord is not None and self.y_coord is not None:
             self.img_type = 'map'
             render_map.Map(self.dataframe).render(self.x_coord,
-                                                  self.y_coord)
-        # else:
-        #     render_plot.render(self.dataframe)
+                                                  self.y_coord,
+                                                  self.z_coord,
+                                                  self.t_coord)
+        # If we have just a time coord then we can make a timeseries:
+        elif self.x_coord is None and self.y_coord is None:
+            self.img_type = 'timeseries'
+            render_plot.Plot(self.dataframe).render_timeseries()
+        # If we don't have any coords then something's gone wrong and we can't
+        # plot anything:
+        elif (self.x_coord and self.y_coord and self.z_coord and self.t_coord) \
+                is None:
+            raise ValueError('All dimension coordinates are either missing or '
+                             'scalar, please choose a dataset with more '
+                             'coordinate points.')
+
+
+
